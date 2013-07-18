3790004848035e9b8eb96956b5f8829122404a6b
Niklas Kamenisch <niklas@Niklas-MC.(none)>
2013-07-18 16:58:21 +0200

    Test

---
+++ src/me/tekkitnerds/controlcenter/beruf/main.java
@@ -0,0 +1,17 @@
+package me.tekkitnerds.controlcenter.beruf;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class main extends me.tekkitnerds.controlcenter.abst_modul {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
+        
+        
+        return true;
+    }
+    
+
+    
+}
